2ab655c6103d0d8b4394b8c22c20633843479e43
RWADEX <amivunati145@gmail.com>
2025-01-16 21:15:46 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RWX DEX</h1>
+        <p>
+          Decentralized Exchange for Real World Assets
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
